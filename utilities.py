bfd56ad7161539b2af95c95a21c30574a5f93eeb
Kantaka-dev <kantaka.dev@gmail.com>
2023-05-05 21:39:19 +0700

    :sparkles: Add Epoching

--- utilities.py
+++ utilities.py
@@ -7,7 +7,8 @@ from mnelab.io.xdf import read_raw_xdf
 import matplotlib.pyplot as plt
 from pprint import pprint
 
-def read_xdf(filename, show_plot=True, show_psd=True, verbose=False) -> mne.io.array.array.RawArray:
+
+def read_xdf(filename, show_plot=True, show_psd=True, verbose=False, plot_scale=169) -> mne.io.array.array.RawArray:
     """
     Loading XDF file into MNE-RawArray. MNE-Python does not support this file format out of the box, 
     but we can use the pyxdf package and MNELAB to import the data. 
@@ -43,7 +44,7 @@ def read_xdf(filename, show_plot=True, show_psd=True, verbose=False) -> mne.io.a
         raw.plot(
             duration=15, 
             start=0, 
-            scalings=169, # You may edit scalings value later
+            scalings=plot_scale, # You may edit scalings value later
         ) #, n_channels=8, bad_color='red'
         show = True
 
@@ -67,9 +68,52 @@ def read_xdf(filename, show_plot=True, show_psd=True, verbose=False) -> mne.io.a
     return raw
 
 
+def show_epoch(raw, plot_scale=169):
+    raw_eeg = raw.pick_channels([
+                    'obci_eeg1_1',
+                    'obci_eeg1_2',
+                    'obci_eeg1_3',
+                    'obci_eeg1_4',
+                    'obci_eeg1_5',
+                ])
+
+    events, event_dict = mne.events_from_annotations(raw_eeg)
+
+    epochs = mne.Epochs(raw_eeg, events, 
+        tmin=0,     # init timestamp of epoch (0 means trigger timestamp same as event start)
+        tmax=10,    # final timestamp (10 means set epoch duration 10 second)
+        baseline=(0, 0),
+        preload=True,
+    )
+
+    # Visualization
+    raw_eeg.plot(
+            duration=10, 
+            start=0, 
+            scalings=plot_scale, # You may edit scalings value later
+        ) #, n_channels=8, bad_color='red'
+        # show = True
+
+    epochs['2'].plot(
+        scalings=plot_scale, # You may edit scalings value later
+        title='Left stimuli start',
+    )
+    epochs['5'].plot(
+        scalings=plot_scale, # You may edit scalings value later
+        title='Right stimuli start',
+    )
+
+    plt.show()
+
+
 if __name__=='__main__':
 
     # raw = read_xdf("example.xdf")
     # raw = read_xdf("test01_OpenBCI.xdf", show_plot=True, show_psd=True)
 
-    raw = read_xdf("omz1.xdf", show_plot=True, show_psd=False)
+    raw = read_xdf("omz1.xdf", 
+            show_plot=False, 
+        show_psd=False,
+    )
+
+    show_epoch(raw)
