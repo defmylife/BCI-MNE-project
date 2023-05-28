77e02e1a9d2e7851518929fd3ef8d6e15bf602a7
pipogood <82525600+pipogood@users.noreply.github.com>
2023-05-28 21:10:48 +0700

    Add realtime file

--- Experiment/Online/RealtimeWithModel.py
+++ Experiment/Online/RealtimeWithModel.py
@@ -3,8 +3,7 @@
 from pylsl import StreamInlet, resolve_stream
 from scipy import signal
 import numpy as np
-
-from models import FFT_CNNModel
+from models import CNNModel
 from gripper import Gripper
 
 def main():
@@ -17,12 +16,13 @@ def main():
     eeg_inlet = StreamInlet(eeg_streams[0])
 
     time_count = 0.0
-    epoch_5s = np.zeros(shape=(3,1250))
+    epoch_5s = np.zeros(shape=(5,1250))
     temp = []
 
-    CNN = FFT_CNNModel(input_channels=3)
+    CNN = CNNModel()
+    CNN.load_weights('CNNN_Mod_mix.h5')
 
-    grip = Gripper()
+    # grip = Gripper()
 
     while True:
         eeg_sample, timestamps = eeg_inlet.pull_sample()
@@ -32,28 +32,32 @@ def main():
             time_count += 1 * 0.004
             time_count = np.round(time_count ,3)
 
-            data = [eeg_sample[1],eeg_sample[2],eeg_sample[3]]
+            data = [eeg_sample[1],eeg_sample[2],eeg_sample[3],eeg_sample[4],eeg_sample[5]]
             temp.append(data)
-            # print(data)
+       
             if time_count == 1.0:
                 temp = np.asarray(temp)
                 epoch_5s = np.delete(epoch_5s, slice(0,250), axis =1)
                 epoch_5s = np.append(epoch_5s, temp.T, axis= 1)
                 send_epoch = epoch_5s
-                predict_raw_value, predict_value = CNN.model_predict_classes(np.reshape(send_epoch, (1, 3, 1250)))
                 print('-----------------------------------------------------------------------------------')
-                print(f'Predict val : 6 Hz Left [{predict_raw_value[0][0]*100.0:.2f} %] 10 Hz Right [{predict_raw_value[0][1]*100.0:.2f} %] Idle [{predict_raw_value[0][2]*100.0:.2f} %]')
-                print(f'Predicted class: {predict_value}')
-                
-                if predict_value[0] == 0:
-                    grip.open_gripper()
-                elif predict_value[0] == 1:
-                    grip.close_gripper()
+                print(send_epoch)
 
+                # predict_raw_value, predict_value = CNN.model_predict_classes(np.reshape(send_epoch, (1, 5, 1250)))
+                # # print(f"predict raw val {predict_raw_value}")
+                # # print(f'predict val {predict_value}')
+                # print('-----------------------------------------------------------------------------------')
+                # print(f'Predict val : 6Hz Left [{predict_raw_value[0][0]*100.0:.2f} %] 10Hz Right [{predict_raw_value[0][1]*100.0:.2f} %] Idle [{predict_raw_value[0][2]*100.0:.2f} %]')
+                # print(f'Predicted class: {predict_value}')
+
+                # if predict_value[0] == 0:
+                #     grip.open_gripper()
+                # elif predict_value[0] == 1:
+                #     grip.close_gripper()
+                
                 temp = np.ndarray.tolist(temp)
                 temp = []
                 time_count = 0.0
           
 if __name__ == '__main__':
     main()
-
